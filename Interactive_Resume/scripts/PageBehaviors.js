c8c2e2054dcd506ccc90a2da8d2a053cf64329c0
Unknown <gordon.lee.6170@gmail.com>
2018-05-10 13:07:06 -0400

    new images for projects

--- Interactive_Resume/scripts/PageBehaviors.js
+++ Interactive_Resume/scripts/PageBehaviors.js
@@ -1,2 +1,65 @@
 // JavaScript Document
 
+function boat3DOverlayOn(){
+	document.getElementById("boat3DFrame").style.display = "block";
+}
+
+function boat3DOverlayOff(){
+	document.getElementById("boat3DFrame").style.display = "none";
+}
+
+function tRex3DOverlayOn(){
+	document.getElementById("tRex3DFrame").style.display = "block";
+}
+
+function tRex3DOverlayOff(){
+	document.getElementById("tRex3DFrame").style.display = "none";
+}
+
+function adHero3DOverlayOn(){
+	document.getElementById("adHero3DFrame").style.display = "block";
+}
+
+function adHero3DOverlayOff(){
+	document.getElementById("adHero3DFrame").style.display = "none";
+}
+
+function adEngineer3DOverlayOn(){
+	document.getElementById("adEngineer3DFrame").style.display = "block";
+}
+
+function adEngineer3DOverlayOff(){
+	document.getElementById("adEngineer3DFrame").style.display = "none";
+}
+
+function adZombie3DOverlayOn(){
+	document.getElementById("adZombie3DFrame").style.display = "block";
+}
+
+function adZombie3DOverlayOff(){
+	document.getElementById("adZombie3DFrame").style.display = "none";
+}
+
+function deceptaconOverlayOn(){
+	document.getElementById("deceptaconFrame").style.display = "block";
+}
+
+function deceptaconOverlayOff(){
+	document.getElementById("deceptaconFrame").style.display = "none";
+}
+
+function redaOverlayOn(){
+	document.getElementById("redaFrame").style.display = "block";
+}
+
+function redaOverlayOff(){
+	document.getElementById("redaFrame").style.display = "none";
+}
+
+function theHutongHunterOverlayOn(){
+	document.getElementById("theHutongHunterFrame").style.display = "block";
+}
+
+function theHutongHunterOverlayOff(){
+	document.getElementById("theHutongHunterFrame").style.display = "none";
+}
