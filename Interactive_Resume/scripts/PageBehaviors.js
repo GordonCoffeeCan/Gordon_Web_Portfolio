e3de5a057ff8a4dfac74ebb3b5e22304c386386b
Unknown <gordon.lee.6170@gmail.com>
2018-05-12 00:38:21 -0400

    recent project on home page

--- Interactive_Resume/scripts/PageBehaviors.js
+++ Interactive_Resume/scripts/PageBehaviors.js
@@ -40,6 +40,22 @@ function adZombie3DOverlayOff(){
 	document.getElementById("adZombie3DFrame").style.display = "none";
 }
 
+function mpmpOverlayOn(){
+	document.getElementById("mpmpFrame").style.display = "block";
+}
+
+function mpmpOverlayOff(){
+	document.getElementById("mpmpFrame").style.display = "none";
+}
+
+function fashionWebsiteOverlayOn(){
+	document.getElementById("fashionWebsiteFrame").style.display = "block";
+}
+
+function fashionWebsiteOverlayOff(){
+	document.getElementById("fashionWebsiteFrame").style.display = "none";
+}
+
 function deceptaconOverlayOn(){
 	document.getElementById("deceptaconFrame").style.display = "block";
 	$(function(){
@@ -49,6 +65,22 @@ function deceptaconOverlayOn(){
 
 function deceptaconOverlayOff(){
 	document.getElementById("deceptaconFrame").style.display = "none";
+}
+
+function cellMetroOverlayOn(){
+	document.getElementById("cellMetroFrame").style.display = "block";
+}
+
+function cellMetroOverlayOff(){
+	document.getElementById("cellMetroFrame").style.display = "none";
+}
+
+function alienConceptOverlayOn(){
+	document.getElementById("alienConceptFrame").style.display = "block";
+}
+
+function alienConceptOverlayOff(){
+	document.getElementById("alienConceptFrame").style.display = "none";
 }
 
 function redaOverlayOn(){
